c79fa540999cc6016dea81f3e87bee74135b4982
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-07-30 17:38:25 +0400

    build drawer page

--- flutter_sidemenu/lib/main.dart
+++ flutter_sidemenu/lib/main.dart
@@ -7,19 +7,54 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(
-        color: Colors.blue,
-        child: SafeArea(
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text('Title'),
-              elevation: 0.0,
+      home: Home(title: 'Drawer Demo'),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  final String title;
+
+  Home({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(child: Text('My Page!')),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
             ),
-            body: null,
-          ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
     );
